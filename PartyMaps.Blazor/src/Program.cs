e1104a99626ebf0d2fbcf2addf478c407baf67d1
Gabriel <gabriel.maurici@hotmail.com>
2023-07-04 02:40:08 -0300

    removido dados sensiveis

--- PartyMaps.Blazor/src/Program.cs
+++ PartyMaps.Blazor/src/Program.cs
@@ -7,7 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 
 builder.Services.AddScoped<IntegrationPartyMapsApiService>();
@@ -15,7 +14,7 @@ builder.Services.AddScoped<IntegrationPartyMapsApiService>();
 builder.Services.AddOidcAuthentication(options =>
 {
     options.ProviderOptions.Authority = "https://accounts.google.com/";
-    options.ProviderOptions.ClientId = "649575633025-pltlppv6mh2onggkumms6n44iul9umsa.apps.googleusercontent.com";
+    options.ProviderOptions.ClientId = "";
     options.ProviderOptions.RedirectUri = "http://localhost:8000/authentication/login-callback";
     options.ProviderOptions.PostLogoutRedirectUri = "http://localhost:8000/authentication/logout-callback";
     options.ProviderOptions.ResponseType = "id_token";
